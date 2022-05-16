ddf57eab105bf97daba8a8e2d08fd54bd32f73a3
nathalie <nathalie@data-terrae.fr>
2022-05-16 10:09:42 +0200

    :wrench: conf files for docker compose

--- webapp/src/captcha_cracker_client.py
+++ webapp/src/captcha_cracker_client.py
@@ -2,15 +2,21 @@
 Function that sends a request to the MLflow model REST API to get a prediction from some input data.
 """
 
-import requests
+import os
+from pathlib import Path
 
-MODEL_HOST = 'https://captchacracker.herokuapp.com'
-MODEL_HOST = 'http://127.0.0.1:8000'
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env', override=False)
+MODEL_HOST = os.getenv('MODEL_HOSTNAME')
+MODEL_PORT = os.getenv('MODEL_PORT')
 
 
 def crack_captcha(img_file) -> str:
     try:
-        response = requests.post(url=f'{MODEL_HOST}/crack', files={'file': img_file})
+        response = requests.post(url=f'{MODEL_HOST}:{MODEL_PORT}/crack', files={'file': img_file})
         response.raise_for_status()
         output = response.json()['captcha_solution']
     except (requests.HTTPError, IOError) as err:
