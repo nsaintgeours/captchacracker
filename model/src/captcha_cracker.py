c5c6c566535fcf0f1f3861a38b1e1d30ce8058bc
nathalie <nathalie@data-terrae.fr>
2022-05-17 10:25:12 +0200

    :alembic: improve image processing

--- model/src/captcha_cracker.py
+++ model/src/captcha_cracker.py
@@ -8,19 +8,26 @@ import cv2
 import numpy as np
 from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 
-OCR_MODEL_CHECKPOINT = 'microsoft/trocr-small-printed'
+OCR_MODEL_CHECKPOINT = 'microsoft/trocr-large-printed'
 OCR_IMG_PROCESSOR = TrOCRProcessor.from_pretrained(OCR_MODEL_CHECKPOINT)
 OCR_MODEL = VisionEncoderDecoderModel.from_pretrained(OCR_MODEL_CHECKPOINT)
 
 
 class CaptchaCracker(object):
 
-    def crack(self, image_bytes: bytes) -> str:
+    def crack(self, image_bytes: bytes, verbose: bool = False) -> str:
         """ Cracks captcha image and returns the colored word. """
         input_img = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
         clustered_img = self._cluster_image(img=input_img)
         cropped_img = self._crop_smallest_cluster(img=clustered_img)
         word = self._optical_character_recognition(img=cropped_img)
+
+        if verbose:
+            cv2.imshow('Input image', input_img)
+            cv2.imshow('Clustered image', cv2.cvtColor(80 * np.uint8(clustered_img), cv2.COLOR_GRAY2RGB))
+            cv2.imshow('Cropped image', cropped_img)
+            cv2.waitKey()
+
         return word
 
     @staticmethod
@@ -35,7 +42,7 @@ class CaptchaCracker(object):
         Returns: (np.ndarray) gray-scale image with shape (width, height);
          Output pixels have integer values ranging from 0 to (n-1) that indicate cluster id.
         """
-        flat_matrix = np.float32(np.array(img).reshape((-1, 3)))
+        flat_matrix = np.float32(np.array(cv2.cvtColor(img[:, :, :3], cv2.COLOR_RGB2HLS)).reshape((-1, 3)))
         _, cluster_ids, _ = cv2.kmeans(
             data=flat_matrix,
             K=n,
@@ -85,6 +92,6 @@ class CaptchaCracker(object):
 
 if __name__ == '__main__':
     cracker = CaptchaCracker()
-    with open(Path(__file__).parent.parent.parent / 'data' / 'sample.jpg', 'rb') as file:
+    with open(Path(__file__).parent.parent.parent / 'data' / 'sample5.png', 'rb') as file:
         text = cracker.crack(image_bytes=file.read())
         print(text)
